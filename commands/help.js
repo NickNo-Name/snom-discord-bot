c70ecb16cdf084876b3ac801da602c3e391a8d95
NickNo-Name <68885078+NickNo-Name@users.noreply.github.com>
2020-10-06 16:11:06 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'help',
 	description: 'Help Command',
 	execute(client, Discord, fs, prefix, message, args, RedditSimple) {
-		message.channel.send(`Type ${prefix}snom for a random Non-NSFW image from r/snom on Reddit\n\nYou can also type ${prefix}snominfo for credits to the creators and such`);
+		message.channel.send(`Type ${prefix}snom for a random Non-NSFW image from r/snom on Reddit\n\nYou can also type ${prefix}info for credits to the creators and such`);
 	},
 };
